6ef02e03b5082390ad04a992194e0d08e3c1f981
孙泽宇 <sunzeyu@thinkingdata.cn>
2021-10-14 14:33:19 +0800

    Release v1.2.2
    Signed-off-by: 孙泽宇 <sunzeyu@thinkingdata.cn>

--- README.md
+++ README.md
@@ -13,19 +13,19 @@ npm install thinkingdata-node --save
 #### 2. 创建 SDK 实例
 首先在代码文件开头引入 `thinkingdata-node`:
 ```js
-var ThinkingAnalytics = require('thinkingdata-node')
+let ThinkingAnalytics = require('thinkingdata-node')
 ```
 
 为使用 SDK 上传数据，需首先初始化 SDK. 我们提供了三种初始化方法:
 ```js
 // Debug Mode: 逐条发送，并返回详细的错误信息
-var ta = ThinkingAnalytics.initWithDebugMode('APP-ID', 'https://SERVER_URL');
+let ta = ThinkingAnalytics.initWithDebugMode('APP-ID', 'https://SERVER_URL');
 
 // Batch Mode：批量发送到接收端
-var ta = ThinkingAnalytics.initWithBatchMode('APP-ID', 'https://SERVER_URL');
+let ta = ThinkingAnalytics.initWithBatchMode('APP-ID', 'https://SERVER_URL');
 
 // Logging Mode: 将数据写入本地日志文件，需要配合 LogBus 使用
-var ta = ThinkingAnalytics.initWithLoggingMode('/PATH/TO/DATA');
+let ta = ThinkingAnalytics.initWithLoggingMode('/PATH/TO/DATA');
 ```
 
 三种模式说明如下:
@@ -37,7 +37,7 @@ var ta = ThinkingAnalytics.initWithLoggingMode('/PATH/TO/DATA');
 Batch Mode 会先将数据存放在缓冲区中，当数据条数超过设定的值(batchSize, 默认为20)，触发上报. 您也可以在初始化 SDK 的时候指定传入配置:
 ```js
 // 初始化 SDK, 指定接收端地址、APP ID、缓冲区大小
-var ta = ThinkingAnalytics.initWithBatchMode('APP-ID', 'https://SERVER_URL', {
+let ta = ThinkingAnalytics.initWithBatchMode('APP-ID', 'https://SERVER_URL', {
 	batchSize: 10, // 缓存数据到达 10 条时触发上报
 	enableLog: true // 允许打印发送数据. 当开启日志的时候会打印详细的发送日志
 	});
@@ -54,7 +54,7 @@ var ta = ThinkingAnalytics.initWithBatchMode('APP-ID', 'https://SERVER_URL', {
 
 ```js
 // 初始化 SDK，按小时切分日志文件.
-var ta = ThinkingAnalytics.initWithLoggingMode('.', { rotateHourly: true});
+let ta = ThinkingAnalytics.initWithLoggingMode('.', { rotateHourly: true});
 ```
 
 #### 3. 上报数据
@@ -66,7 +66,7 @@ SDK 初始化完成后，后续即可使用 ta 的接口来上报数据.
 您可以调用 track 来上传事件，建议您根据预先梳理的文档来设置事件的属性以及发送信息的条件。上传事件示例如下：
 ```js
 // 定义事件数据
-var event = {
+let event = {
 	// 账号 ID （可选)
 	accountId: 'node_test',
 	// 访客 ID （可选)，账号 ID 和访客 ID 不可以都为空
@@ -115,7 +115,7 @@ ta.track(event, true);
 
 // 设置动态公共属性
 ta.setDynamicSuperProperties(()=>{
-    var date = new Date();
+    let date = new Date();
     date.setYear(2018);
     return {
         super_date: date,
@@ -137,7 +137,7 @@ ta.clearSuperProperties();
 对于一般的用户属性，您可以调用 userSet 来进行设置. 使用该接口上传的属性将会覆盖原有的属性值，如果之前不存在该用户属性，则会新建该用户属性:
 ```js
 // 用户属性数据
-var userData = {
+let userData = {
 	// 账号 ID （可选)
 	accountId: 'node_test',
 	// 访客 ID （可选)，账号 ID 和访客 ID 不可以都为空
--- example.js
+++ example.js
@@ -1,12 +1,12 @@
-var ThinkingAnalytics = require('./lib/thinkingdata-node');
+let ThinkingAnalytics = require('./lib/thinkingdata-node');
 //只能初始化一次
 
 //选择是否压缩
 consumerConfig = {
-    compress : false
+    compress: false
 };
 /*
-var taBatch = ThinkingAnalytics.initWithBatchMode('appid', 'URL', {
+let taBatch = ThinkingAnalytics.initWithBatchMode('appid', 'URL', {
     batchSize: 2,
     enableLog: true,
     compress :false//默认true 代表gzip压缩，false适合在内网设置
@@ -16,68 +16,92 @@ var taBatch = ThinkingAnalytics.initWithBatchMode('appid', 'URL', {
 config = {
     dryRun: false
 };
-//var taDebug = ThinkingAnalytics.initWithDebugMode('appid', 'URL',config);
+//let taDebug = ThinkingAnalytics.initWithDebugMode('appid', 'URL',config);
 
-var taLogging = ThinkingAnalytics.initWithLoggingMode('.'); //, { rotateHourly: true, });
+let taLogging = ThinkingAnalytics.initWithLoggingMode('.', {filePrefix: 'test'}); //, { rotateHourly: true, });
 
-var ta = taLogging;
+let ta = taLogging;
 
-var event = {
-  accountId: 'node_test',
-  distinctId: 'node_distinct_id',
-  event: 'test_event',
-  time: new Date(),
-  ip: '202.38.64.1',
-  properties: {
-    prop_date: new Date(),
-    prop_double: 134.1,
-    prop_string: 'hello world',
-    prop_int: 67,
-  },
-  callback(e) {
-    if (e) {
-      console.log(e);
+let event = {
+    accountId: '',
+    distinctId: 'a123213213213a123213213213a123213213213a123213213213a123213213213a123213213213a123213213213a123213213213a123213213213a123213213213',
+    event: 'test_event',
+    time: new Date(),
+    ip: '202.38.64.1',
+    properties: {
+        prop_date: new Date(),
+        prop_double: 134.1,
+        prop_string: 'hello world',
+        prop_int: 67,
+    },
+    callback(e) {
+        if (e) {
+            console.log(e);
+        }
     }
-  }
 };
 
 // 设置动态公共属性
 ta.setDynamicSuperProperties(() => {
-  var date = new Date();
-  date.setYear(2018);
-  return {
-    super_date: date,
-    super_int: 5,
-  }
+    let date = new Date();
+    date.setYear(2018);
+    return {
+        super_date: date,
+        super_int: 5,
+    }
 });
 
 // 设置公共事件属性
 ta.setSuperProperties({
-  super_int: 8, // 不会出现在最终上报数据，因为会被动态公共属性覆盖.
-  super_debug_string: 'hahahaha',
+    super_int: 8, // 不会出现在最终上报数据，因为会被动态公共属性覆盖.
+    super_debug_string: 'hahahaha',
 });
 
 
-for (var i = 0; i < 5; i++) {
-  event.event = event.event + i;
-  ta.track(event);
+for (let i = 0; i < 5; i++) {
+    event.event = event.event + i;
+    ta.track(event);
 }
 
 
-var userData = {
-  distinctId: 'node_test',
-  properties: {
-    prop_date: new Date(),
-    prop_double: 134.12,
-    prop_string: 'hello',
-    prop_int: 666,
-    prop_array:['str1','str2'],
-  },
-  callback(e) {
-    if (e) {
-      console.log(e);
+let userData = {
+    distinctId: 'node_test',
+    uuid: 'f5394eef-e576-4709-9e4b-a7c231bd34a4',
+    properties: {
+        prop_date: new Date(),
+        prop_double: 134.12,
+        prop_string: 'hello',
+        prop_int: 666,
+        prop_array: ['str1', 'str2'],
+        data: {
+            "_id": "GPA.3311-4836-3043-35961",
+            "platform": "googleplay",
+            "productId": "com.superant.tripeaksadventure.level_sale",
+            "purchaseToken": "okhgglkdpbfeiphjchdioedn.AO-J1OxesPpm03Zz2rb05o7Og-32xUzVaHD7Tpe3zXnA0s7OyFlJCblb9M8mYDx3ulIPSxvPgAH82Qbu6-oTB7X6i-jlQaNbdftx5EjxG8dIbjJcqYm9ufA",
+            "purchaseTokenOriginal": "okhgglkdpbfeiphjchdioedn.AO-J1OxesPpm03Zz2rb05o7Og-32xUzVaHD7Tpe3zXnA0s7OyFlJCblb9M8mYDx3ulIPSxvPgAH82Qbu6-oTB7X6i-jlQaNbdftx5EjxG8dIbjJcqYm9ufA",
+            "coin": 12000,
+            "numPrice": 1.99,
+            "group": 1,
+            "couponId": -1,
+            "canPlayLevel": 5425,
+            "currentLevel": 4735,
+            "isRandomLevel": 0,
+            "status": "completed",
+            "client_status": "uncompleted",
+            "userId": "UzWxTsS*8",
+            "datetime": "2021-04-23T09:22:12.691Z",
+            "date": {
+                "year": 2021,
+                "month": 4,
+                "day": 23
+            }
+        },
+    },
+    callback(e) {
+        if (e) {
+            console.log(e);
+        }
     }
-  }
 };
 
 // 设置用户属性
@@ -85,11 +109,11 @@ ta.userSet(userData);
 
 // 累加用户属性
 ta.userAdd({
-  accountId: 'node_test',
-  properties: {
-    prop_double: 0.6,
-    prop_int: 222,
-  }
+    accountId: 'node_test',
+    properties: {
+        prop_double: 0.6,
+        prop_int: 222,
+    }
 });
 
 
@@ -101,8 +125,8 @@ ta.userUnset({
 
 ta.userAppend({
     accountId: 'node_test',
-    properties:{
-      prop_array:['str3','str4']
+    properties: {
+        prop_array: ['str3', 'str4']
     }
 });
 
